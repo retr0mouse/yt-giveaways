0548e9c8616ef9643a174506288294737f2efa1b
Daniil <daniil.sharin667@gmail.com>
2024-02-15 17:20:34 +0200

    Update backend URL and add clear script for deployment

--- src/client/api/YoutubeApi.ts
+++ src/client/api/YoutubeApi.ts
@@ -1,6 +1,6 @@
 import { DisplayedComment } from "../types/DisplayedComment";
 
-const BACKEND_URL = "https://yt-giveaways-fb3ebf590975.herokuapp.com/";
+const BACKEND_URL = "https://yt-giveaways-fb3ebf590975.herokuapp.com";
 
 export class YoutubeApi {
     static async getRandomComment(videoUrl: string): Promise<DisplayedComment> {
